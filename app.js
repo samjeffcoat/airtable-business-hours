36881715aaac6e520583aed6c48122707d96cc2d
Sam <samjeffcoat@gmail.com>
2021-06-14 22:12:05 -0500

    app built with caching

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const base = require("airtable").base("appbJvnqfqumofJAg")
+const express = require("express")
+const path = require("path")
+const app = express()
+app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "."))
 
+let records
+app.get('/', async (req, res) => { 
+    if (records) {
+    console.log("cached")
+    res.render("page", {
+        records,
+    }) 
+    }   else {
+        (async () => {
+            records = await base("Business Hours")
+            .select({
+                view: "Grid view",
+            })
+            .firstPage()
 
-
-; (async () => {
-    const records = await base("Business Hours")
-    .select({
-        view: "Grid view",
-    })
-    .firstPage()
-    for(const record of records) {
-        console.log(record.get("Day"), record.get("Hours"))
+            res.render("page", {
+                records,
+            })
+            setTimeout(() => {
+                records = null
+            }, 10 * 1000)
+        })()
     }
-
-})()
+})
+app.listen(3000, () => console.log("Server Ready"))
